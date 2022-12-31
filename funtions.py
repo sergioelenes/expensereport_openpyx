eb954f70f649adcb5491a81bdb57ab16cd765982
sergioelenes <87728823+sergioelenes@users.noreply.github.com>
2022-12-31 10:54:47 -0700

    .

--- funtions.py
+++ funtions.py
@@ -9,8 +9,8 @@ courrent_dir = os.getcwd()
 dabase = 'expensedb.xlsx'
 print(os.path.exists(dabase))
 df = pd.read_excel('expensedb.xlsx', sheet_name='detail')
-#df.style.format({'Amount': '${0:,.0f}'})
-#pivot = df.pivot_table(values='Amount', index='Concept', columns='Month', aggfunc='sum', fill_value="-", margins=True, margins_name='Total')
+pd.DataFrame(df)
+df.style.format({'Amount': "${:,.2f}"})
+pivot = df.pivot_table(values='Amount', index='Concept', columns='Month', aggfunc='sum', fill_value="-", margins=True, margins_name='Total')
 
-
-print(courrent_dir)
+print(df)
